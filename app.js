70e309a37232efe7ce4f8bde1f1a7d4ed2c85ae3
arfkrn <aripk2020@gmail.com>
2024-12-11 07:44:16 +0700

    ga tau

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import router from "./src/routes/routes.js";
 import admin from "./src/routes/admin.js";
 
 const app = express();
 const port = 4000;
-
+const corsOptions = {
+    origin: "http://localhost:5173",
+};
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use("/api/v1", router);
 app.use("/api/v1/admin", admin);
 
 const server = app.listen(port, () => {
-  console.log("Server running on port: ", port);
+    console.log("Server running on port: ", port);
 });
 
 const gracefullShutdown = async () => {
-  console.log("Shutting down...");
-  server.close(() => {
-    console.log("Http server closed");
-  });
+    console.log("Shutting down...");
+    server.close(() => {
+        console.log("Http server closed");
+    });
 
-  // try {
-  //   await prisma.$disconnect();
-  //   console.log("Database connection closed");
-  // } catch (err) {
-  //   console.log("Error closing database connection: ", err);
-  // }
+    // try {
+    //   await prisma.$disconnect();
+    //   console.log("Database connection closed");
+    // } catch (err) {
+    //   console.log("Error closing database connection: ", err);
+    // }e
 
-  process.exit(0);
+    process.exit(0);
 };
 
 process.on("SIGINT", gracefullShutdown);
 process.on("SIGTERM", gracefullShutdown);
+
+export default app;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,97 +4,103 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const inputValidationSchema = Joi.object({
-  username: Joi.string().required(),
-  password: Joi.string().required(),
+    username: Joi.string().required(),
+    password: Joi.string().required(),
 }).options({ abortEarly: false });
 
 const prisma = new PrismaClient();
 
 const login = async (req, res) => {
-  const { error } = inputValidationSchema.validate(req.body);
-  if (error) {
-    const validationErrors = error.details.map((error) => {
-      return error.message.replace(/['"]/g, "");
-    });
+    const { error } = inputValidationSchema.validate(req.body);
+    if (error) {
+        const validationErrors = error.details.map((error) => {
+            return error.message.replace(/['"]/g, "");
+        });
 
-    return res
-      .status(400)
-      .json({ success: false, data: null, errors: validationErrors });
-  }
-
-  const { username, password } = req.body;
-
-  try {
-    const user = await prisma.users.findUnique({
-      where: {
-        username,
-      },
-      select: {
-        username: true,
-        password: true,
-      },
-    });
-
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({
-        success: false,
-        data: null,
-        errors: "Username or Password invalid!",
-      });
+        return res
+            .status(400)
+            .json({ success: false, data: null, errors: validationErrors });
     }
 
-    const token = jwt.sign(
-      { username: user.username },
-      process.env.JWT_SECRET,
-      { expiresIn: "2h" }
-    );
+    const { username, password } = req.body;
 
-    res.status(200).json({ success: true, token: token, errors: null });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      token: null,
-      errors: error.message,
-    });
-  }
+    try {
+        const user = await prisma.users.findUnique({
+            where: {
+                username,
+            },
+            select: {
+                username: true,
+                password: true,
+            },
+        });
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({
+                success: false,
+                data: null,
+                errors: "Username or Password invalid!",
+            });
+        }
+
+        const token = jwt.sign(
+            { username: user.username },
+            process.env.JWT_SECRET,
+            { expiresIn: "2h" }
+        );
+
+        const data = { username: user.username, token };
+
+        res.status(200).json({ success: true, data, errors: null });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            token: null,
+            errors: error.message,
+        });
+    }
 };
 
 const register = async (req, res) => {
-  const { error } = inputValidationSchema.validate(req.body);
-  if (error) {
-    const validationErrors = error.details.map((error) => {
-      return error.message.replace(/['"]/g, "");
-    });
+    const { error } = inputValidationSchema.validate(req.body);
+    if (error) {
+        const validationErrors = error.details.map((error) => {
+            return error.message.replace(/['"]/g, "");
+        });
 
-    return res
-      .status(400)
-      .json({ success: false, data: null, errors: validationErrors });
-  }
+        return res
+            .status(400)
+            .json({ success: false, data: null, errors: validationErrors });
+    }
 
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  try {
-    const user = await prisma.users.create({
-      data: {
-        username,
-        password: hashedPassword,
-      },
-      select: {
-        username: true,
-      },
-    });
+    try {
+        const user = await prisma.users.create({
+            data: {
+                username,
+                password: hashedPassword,
+            },
+            select: {
+                username: true,
+            },
+        });
 
-    res.status(201).json({ success: true, data: user.username, errors: null });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      token: null,
-      errors: error.message,
-    });
-  }
+        res.status(201).json({
+            success: true,
+            data: user.username,
+            errors: null,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            token: null,
+            errors: error.message,
+        });
+    }
 };
 
 export { login, register };
